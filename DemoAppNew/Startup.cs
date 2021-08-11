8d6936125732d2b8d680dbe94d029ba447b6b454
Bakul Jain <BJain2@slb.com>
2021-08-11 17:32:09 +0530

    Added few !!

--- DemoAppNew/Startup.cs
+++ DemoAppNew/Startup.cs
@@ -27,7 +27,7 @@ namespace DemoAppNew
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World! - My new app deployed in Azure");
+                await context.Response.WriteAsync("Hello World!!! - My new app deployed in Azure");
             });
         }
     }
